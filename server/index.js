378ef7df96e625de7c75445c53dc34326b8309c6
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-08 02:12:10 -0500

    Final Redux setup

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 // Using Express built-in body parsing instead of body-parser set till 30mb [Middlewares]
-app.use('/posts', postRoutes);
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://bhavyakc08:project14@cluster0.x9pre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5000;
 
