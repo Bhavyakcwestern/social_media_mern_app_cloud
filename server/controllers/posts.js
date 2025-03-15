a0010efe5c11af2701f796833994a4a9a6f89f36
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-15 15:45:52 -0400

    Added controllers and routers for Like Feature(Server side)

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,3 +49,16 @@ export const deletePost=async(req,res)=>{
 
     res.json({message:'Post deleted successfully'});
 }   
+//It is very similar to update post when it will take id and update the likeCount
+//  with moongoose function
+export const likePost=async(req,res)=>{
+    const {id:_id}=req.params;   //eg post/123 =>123
+
+    //We are checking in our database if ID is valid
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    
+    const post =await PostMessage.findById(_id);
+    const updatedPost=await PostMessage.findByIdAndUpdate(_id,{likeCount:post.likeCount + 1},{new:true});
+
+    res.json(updatedPost); //updatePost will have likeCount increased by 1
+}
