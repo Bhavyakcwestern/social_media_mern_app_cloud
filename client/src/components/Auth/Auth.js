a9f0640a3d0ba581674d4386e36e9f48161ecd77
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-16 17:00:40 -0400

    Added Auth component

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { signin, signup } from '../../actions/auth';
+import useStyles from './styles';
+
+const Auth = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState({ name: '', email: '', password: '', confirmPassword: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSignup) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <TextField name="name" label="Name" onChange={handleChange} autoFocus fullWidth />
+            )}
+            <TextField name="email" label="Email Address" onChange={handleChange} type="email" fullWidth />
+            <TextField name="password" label="Password" onChange={handleChange} type="password" fullWidth />
+            {isSignup && <TextField name="confirmPassword" label="Repeat Password" onChange={handleChange} type="password" fullWidth />}
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            {isSignup ? 'Sign Up' : 'Sign In'}
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
