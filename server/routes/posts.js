61e589a5ee662e39ea02c54667b2af4b04fab85a
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-16 16:52:54 -0400

    Added middlewares to post.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,13 @@
-import express from 'express'; 
+import express from 'express';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
-import { getPosts,createPost,updatePost,deletePost,likePost} from '../controllers/posts.js'; 
+const router = express.Router();
 
-const router = express.Router(); 
-
-router.get('/', getPosts); 
-router.post('/',createPost);
-//Updating existing document
-router.patch('/:id',updatePost);
-router.delete('/:id',deletePost);
-router.patch('/:id/likePost',likePost);
+router.get('/', getPosts);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
