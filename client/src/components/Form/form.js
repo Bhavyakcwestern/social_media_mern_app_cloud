adfff61d72f9312613f281f04dd505771447ba24
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-17 13:38:12 -0400

    Important commit-Removed creator from the form and fixed the form on client side

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -10,10 +10,11 @@ import useStyles from './styles';
 import { createPost,updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const [postData, setPostData] = useState({ title: '', message: '', tags: '', selectedFile: '' });
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
+  const user=JSON.parse(localStorage.getItem('profile'));
 
   useEffect(() => {
     if (post) setPostData(post);
@@ -21,18 +22,30 @@ const Form = ({ currentId, setCurrentId }) => {
 
   const clear = () => {
     setCurrentId(0);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setPostData({  title: '', message: '', tags: '', selectedFile: '' });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(currentId){
-      dispatch( updatePost(currentId,postData)); //Will dispatch updatePost Action if currentId is selected
+    if(currentId === 0){
+      dispatch(createPost({...postData,name:user?.result?.name})); //Post Data that we are getting from form.js return type below
+      clear();
     }else{
-      dispatch(createPost(postData)); //Post Data that we are getting from form.js return type below
+      dispatch( updatePost(currentId,{...postData,name:user?.result?.name})); //Will dispatch updatePost Action if currentId is selected
+      clear();
     }
    };
     
+
+   if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like others' memories.
+        </Typography>
+      </Paper>
+    );
+  }
     //   const handleSubmit = async (e) => {
     // setCurrentId(0);
     // setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
@@ -50,7 +63,7 @@ const Form = ({ currentId, setCurrentId }) => {
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ?'Editing the Post':'Create a Post'}</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        {/* <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} /> */}
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 // when user click like button =>auth middleware (NEXT) =>LIKE Controller.....
 const auth = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.headers.Authorization.split(" ")[1];
         if (!token) {
             return res.status(401).json({ message: "No token provided" });
         }
