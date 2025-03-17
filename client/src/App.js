abfc2ff4d91704cac2fdbdfc550d247c3745874e
Bhavya Champaneri <bchampan@uwo.ca>
2025-03-16 20:00:04 -0400

    Added  Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,22 @@
-import React,{useState,useEffect} from 'react';
-import { Container, AppBar, Typography, Grow, Grid, Button } from '@material-ui/core';
-import photos from './images/memories.png';
-import { useDispatch } from 'react-redux';
-
-import Form from './components/Form/form.js';
-import Posts from './components/Posts/posts.js';
-import useStyles from './styles.js';
-import {getPost} from './actions/posts.js' 
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.js';
+import Home from './components/Home/Home.js';
+import Auth from './components/Auth/Auth.js';
 
 const App = () => {
-    const [currentId,setCurrentId]=useState(null);  //Prop passing to Form.js and Posts.js
-    const classes = useStyles();
-    const dispatch=useDispatch();
-
-    //We are mounting our post using dispatch->Action
-    useEffect(()=>{
-        dispatch(getPost());
-    },[currentId,dispatch]);
-    
-    return (
-        <div>
-            <Container maxwidth="lg">
-                <Navbar/>
+    return (  
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/auth' exact component={Auth} />
+                </Switch>
             </Container>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </div>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
