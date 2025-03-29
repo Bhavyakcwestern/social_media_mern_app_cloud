ee306494f50df5c7ba9e3e440758e25e9ce61f13
Yash Gupta <gupta.yash211@gmail.com>
2025-03-29 15:35:12 -0400

    edited_env_url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import store from '../store.js'; // Import the Redux store
+
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+
  
-const API = axios.create({ baseURL: 'http://35.192.132.239' });
+
  
 API.interceptors.request.use((req) => {
   const state = store.getState(); // Get the current state from the Redux store
